606a6e2eda0a10425e31c421e9b1f63398c458e7
LucidLuck <123212702+LucidLuck@users.noreply.github.com>
2026-01-01 03:39:06 -0500

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const res = await context.next();
-  // Basic security headers
-  res.headers.set("X-Content-Type-Options", "nosniff");
-  res.headers.set("X-Frame-Options", "DENY");
-  res.headers.set("Referrer-Policy", "no-referrer");
-  res.headers.set("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
-  return res;
-}
